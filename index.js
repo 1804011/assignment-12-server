cc73619d4a1f2808377cd1c909a3df5c3a5da3d0
1804011 <sharifsharifulislam061@gmail.com>
2022-05-24 18:56:35 +0600

    parts purchase api done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.clear();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,6 +8,38 @@ require("dotenv").config();
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//database connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g5amp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+async function run() {
+	try {
+		await client.connect();
+		app.get("/parts", async (req, res) => {
+			const partsCollection = client.db("assignment-12").collection("parts");
+			const result = await partsCollection.find().toArray();
+
+			res.send(result);
+		});
+		app.get("/parts/:id", async (req, res) => {
+			const { id } = req?.params;
+			const partsCollection = client.db("assignment-12").collection("parts");
+			const result = await partsCollection.findOne({
+				_id: ObjectId(id),
+			});
+
+			res.send(result);
+		});
+	} finally {
+		//await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
 	res.send("welcome to server");
